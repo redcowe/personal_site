b4ab2af496d046e1bcff2055827a61e60f51dacd
redcowe <joshuacowell@hotmail.com>
2022-03-16 13:10:27 -0400

    Added a project, changed fonts, and added overflow for current and recent projects

--- src/routes/Projects.jsx
+++ src/routes/Projects.jsx
@@ -4,11 +4,13 @@ const Projects = () => {
     return (
         <div className="project-wrapper">
             <div className="current-projects">
-                <h2>Current</h2>
-                <ProjectTemplate title="Personal Site" desc="My website" link="https://github.com/redcowe/personal_site"/>
+                <h2 className="current-projects-h2">Current</h2>
+                <ProjectTemplate title="Personal Site" desc="Personal Website" link="https://github.com/redcowe/personal_site"/>
+                <ProjectTemplate title="Learning Java" desc="Repo for my java learning" link="https://github.com/redcowe/LearnJava"/>
             </div>
             <div className="completed-projects">
-                <h2>Recently Completed</h2>
+                <h2 className="completed-projects-h2">Recently Completed</h2>
+                <ProjectTemplate title="Student Roster" desc="A C++ based command-line program" link="https://github.com/redcowe/student-roster"/>
             </div>
         </div>
     )
