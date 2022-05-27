5a2c97b0c6eb5d0a8838cb7ca6113a786e53e4cc
Joshua Cowell <joshuacowell@hotmail.com>
2022-05-27 00:21:00 +0000

    removed right header section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,7 +10,6 @@ export default function Header() {
         <>
             <section className="header">
                 <HeaderLeft />
-                <HeaderRight />
             </section>
         </>
     )
