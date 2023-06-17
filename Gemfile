9ac5474bb43464390e7eac35b6a474b146194a03
redcowe <joshuacowell@hotmail.com>
2023-06-17 02:01:38 +0000

    added routes and started building navigation

--- Gemfile
+++ Gemfile
@@ -72,3 +72,6 @@ group :test do
 end
 
 gem "tailwindcss-rails", "~> 2.0"
+
+gem 'inline_svg'
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  root "home#index"
 
+  root "home#index"
   get "/home", to: "home#index"
+  get 'about', to: "about#index"
+  get 'contact', to: "contact#show"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
